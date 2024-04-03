786a54defc43095d4ce496e8b1e8e7e50405ff21
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-04-03 13:13:50 -0300

    get countries

--- backend/PacketSniffer.py
+++ backend/PacketSniffer.py
@@ -1,6 +1,7 @@
 from scapy.all import sniff
 from scapy.layers.inet import IP
 import threading
+from IpToCountry import IpToCountry
 
 
 class PacketSniffer:
@@ -15,12 +16,13 @@ class PacketSniffer:
     def __start_sniffing(self):
         filter_expression = "ip and tcp"
         while not self.__stop_sniffing.is_set():
-            self.__results = sniff(count=100, filter=filter_expression)
+            print("sniffing 10...")
+            self.__results = sniff(count=10, filter=filter_expression)
+            print(self.__results.show())
             self.__extract_src_and_dst_ips()
             self.__extract_destination_countries()
 
     def __extract_src_and_dst_ips(self):
-        print(self.__results.show())
         for pkt in self.__results:
             if IP in pkt:
                 src_ip = pkt[IP].src
@@ -35,7 +37,13 @@ class PacketSniffer:
                     self.__destination_ips[dst_ip] = 1
 
     def __extract_destination_countries(self):
-        pass
+        ip_to_country = IpToCountry("ip_to_country.csv")
+        for ip in self.__destination_ips:
+            country = ip_to_country.get_country(ip)
+            if country in self.__destination_countries:
+                self.__destination_countries[country] += self.__destination_ips[ip]
+            else:
+                self.__destination_countries[country] = self.__destination_ips[ip]
 
     def start(self):
         self.__sniffer_thread.start()
