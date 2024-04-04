f6a3f1eb5dc1e7f5e5294a708f568800fed11b43
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-04-03 21:46:18 -0300

    Fix country selection and optimize loading of .csv

--- backend/PacketSniffer.py
+++ backend/PacketSniffer.py
@@ -12,38 +12,44 @@ class PacketSniffer:
         self.__destination_countries = {}
         self.__stop_sniffing = threading.Event()
         self.__sniffer_thread = threading.Thread(target=self.__start_sniffing)
+        print("Loading ip to country csv...")
+        self.__ip_to_country = IpToCountry("ip_to_country.csv")
 
     def __start_sniffing(self):
         filter_expression = "ip and tcp"
         while not self.__stop_sniffing.is_set():
-            print("sniffing 10...")
+            print("sniffing 10 packets...")
             self.__results = sniff(count=10, filter=filter_expression)
             print(self.__results.show())
-            self.__extract_src_and_dst_ips()
-            self.__extract_destination_countries()
+            self.__parse_results()
 
-    def __extract_src_and_dst_ips(self):
+    def __parse_results(self):
         for pkt in self.__results:
             if IP in pkt:
                 src_ip = pkt[IP].src
                 dst_ip = pkt[IP].dst
-                if src_ip in self.__source_ips:
-                    self.__source_ips[src_ip] += 1
-                else:
-                    self.__source_ips[src_ip] = 1
-                if dst_ip in self.__destination_ips:
-                    self.__destination_ips[dst_ip] += 1
-                else:
-                    self.__destination_ips[dst_ip] = 1
+                self.__add_source_ip_entry(src_ip)
+                self.__add_destination_ip_entry(dst_ip)
+                self.__add_country_entry(dst_ip)
 
-    def __extract_destination_countries(self):
-        ip_to_country = IpToCountry("ip_to_country.csv")
-        for ip in self.__destination_ips:
-            country = ip_to_country.get_country(ip)
-            if country in self.__destination_countries:
-                self.__destination_countries[country] += self.__destination_ips[ip]
-            else:
-                self.__destination_countries[country] = self.__destination_ips[ip]
+    def __add_country_entry(self, ip: str):
+        country = self.__ip_to_country.get_country(ip)
+        if country in self.__destination_countries:
+            self.__destination_countries[country] += 1
+        else:
+            self.__destination_countries[country] = 1
+
+    def __add_source_ip_entry(self, ip: str):
+        if ip in self.__source_ips:
+            self.__source_ips[ip] += 1
+        else:
+            self.__source_ips[ip] = 1
+
+    def __add_destination_ip_entry(self, ip: str):
+        if ip in self.__destination_ips:
+            self.__destination_ips[ip] += 1
+        else:
+            self.__destination_ips[ip] = 1
 
     def start(self):
         self.__sniffer_thread.start()
